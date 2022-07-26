05a84e0c4007f18a90af3609c544bf3a8c10784d
Kevin <kevin.castro.chavez0@gmail.com>
2022-07-25 18:05:26 -0600

    learned about int, double and float datatypes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿int num1;
+num1 = 13;
+int num2 = 10;
+int num3, num4, num5; // Declaring many variables at the same time
+// num2 = 100; Can be overwritten
+
+int sum = num1 + num2;
+
+Console.WriteLine($"Number 1: {num1} plus Number 2: {num2} is equal to: {sum}");
+
+double d1 = 3.1416;
+double d2 = 5.1;
+double doubleResult = d1 / d2;
+Console.WriteLine(doubleResult); 
+
+float f1 = 3.1416f;
+float f2 = 5.1f; // Declare explicitly that it is a double
+float floatResult = f1 / f2;
+Console.WriteLine(floatResult); 
