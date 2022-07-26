96456b1c9b4cf639fb46aec358a4fb67e38bbbfd
Kevin <kevin.castro.chavez0@gmail.com>
2022-07-25 18:21:31 -0600

    learned about string datatype

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
-﻿int num1;
-num1 = 13;
-int num2 = 10;
-int num3, num4, num5; // Declaring many variables at the same time
-// num2 = 100; Can be overwritten
+﻿// int num1;
+// num1 = 13;
+// int num2 = 10;
+// int num3, num4, num5; // Declaring many variables at the same time
+// // num2 = 100; Can be overwritten
 
-int sum = num1 + num2;
+// int sum = num1 + num2;
 
-Console.WriteLine($"Number 1: {num1} plus Number 2: {num2} is equal to: {sum}");
+// Console.WriteLine($"Number 1: {num1} plus Number 2: {num2} is equal to: {sum}");
 
-double d1 = 3.1416;
-double d2 = 5.1;
-double doubleResult = d1 / d2;
-Console.WriteLine(doubleResult); 
+// double d1 = 3.1416;
+// double d2 = 5.1;
+// double doubleResult = d1 / d2;
+// Console.WriteLine(doubleResult); 
 
-float f1 = 3.1416f;
-float f2 = 5.1f; // Declare explicitly that it is a double
-float floatResult = f1 / f2;
-Console.WriteLine(floatResult); 
+// float f1 = 3.1416f;
+// float f2 = 5.1f; // Declare explicitly that it is a double
+// float floatResult = f1 / f2;
+// Console.WriteLine(floatResult); 
+
+// The string datatype comes from System, using the String class
+string myName = "Kevin";
+string capsString = myName.ToUpper();
+string lowerString = myName.ToLower();
+
+Console.WriteLine(myName);
+Console.WriteLine(capsString);
+Console.WriteLine(lowerString);
